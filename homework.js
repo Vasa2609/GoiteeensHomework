be051fd172f2fefdd2a49606b5618afc6309e05b
Stefurak Vasyl <vasilstifurak303@gmail.com>
2024-04-21 17:53:53 +0300

    дз за 16.11

---
+++ homework.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    let onload_window_zmina = document.getElementById("zmina");
+    let username = prompt("введи ім'я")
+    let password = prompt("введи пароль")
+    onload_window_zmina.innerHTML = "<h1>Hello world!!!!</h1>"
+}
